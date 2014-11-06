58ceb0b481da1566cfc4e7e2fbec6e9e225a2dee
brnikita <nikita-bragin@mail.ru>
2014-11-06 15:25:05 +0300

    Honeypot completed

--- honeypot/config.js
+++ honeypot/config.js
@@ -8,12 +8,5 @@ module.exports = {
      * @name PORT
      * @type {number}
      */
-    PORT: 8000,
-
-    /**
-     * @constant
-     * @name TLS_CIPHERS
-     * @type {number}
-     */
-    TLS_CIPHERS: 'ECDHE-RSA-AES128-SHA256:AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH'
+    PORT: 8000
 };
